d5a71ba84a0ebada28387a3e9458cc2006ec270b
AndyGauge <andygauge@gmail.com>
2018-01-23 21:35:21 -0800

    spree-creditcard

--- app/models/spree/gateway/payeezy.rb
+++ app/models/spree/gateway/payeezy.rb
@@ -9,6 +9,10 @@ module Spree
       ActiveMerchant::Billing::PayeezyGateway
     end
 
+    def payment_source_class
+      Spree::CreditCard
+    end
+
     def purchase(money, creditcard, gateway_options)
       gateway_options[:description] = "Spree Order"
       provider.purchase(money, creditcard, gateway_options)
